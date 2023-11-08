37e248a489f4cd787c2dd2c7788d5b0c27ba12ef
HasanR <hrahimi@360insights.com>
2023-11-08 17:02:16 -0500

    added new components

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 import Header from '@/components/Header'
+import TopCards from '@/components/TopCards'
+import BarChart from '@/components/BarChart'
+import RecentOrders from '@/components/RecentOrders'
 
 export default function Home() {
   return (
@@ -12,6 +15,11 @@ export default function Home() {
       </Head>
       <main className='bg-gray-100'>
         <Header/>
+        <TopCards />
+        <div className='p-4 grid md:grid-cols-3 grid-cols-1 gap-4'>
+          <BarChart/>
+          <RecentOrders />
+        </div>
       </main>
     </>
   )
